a039ebc65ba70eeafceb3f90ee76da703eedd066
3z3qu13l <shawilelol@protonmail.com>
2024-05-30 20:30:32 +0200

    fix: minor fixes

--- linux.sh
+++ linux.sh
@@ -8,6 +8,7 @@ sudo apt install zsh -y
 chsh -s $(which zsh)
 #######################################################################
 # To start using the Z Shell, log out of the terminal and log back in #
+# You can run echo $SHELL from a new terminal to confirm              #
 #######################################################################
 
 # ohmyzsh https://github.com/ohmyzsh/ohmyzsh
@@ -20,7 +21,7 @@ echo "alias ..='cd ..'" >> ~/.zshrc
 echo "alias ...='cd ../..'" >> ~/.zshrc
 
 # bat https://github.com/sharkdp/bat
-sudo apt install bat
+sudo apt install -y bat
 echo "alias cat='batcat' # if doesnt work try alias cat='bat'" >> ~/.zshrc
 
 # eza https://github.com/eza-community/eza/blob/main/INSTALL.md
@@ -28,29 +29,29 @@ sudo mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-sudo apt update
-sudo apt install -y eza
+sudo apt update && sudo apt install -y eza
 echo "alias ll='eza -ll -a --icons=always --group-directories-first'" >> ~/.zshrc
 echo "alias ls='eza -a --icons=always --group-directories-first'" >> ~/.zshrc
+source ~/.zshrc
 
 # powerlevel10k prerequisites
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-sudo cp MesloLGS*.ttf /usr/share/fonts/
-sudo fcache-cache /usr/share/fonts/
+sudo mv MesloLGS*.ttf /usr/share/fonts/
 #######################################################################
 # Manually set terminal font to MesloLGS NF                           #
+# You can also change opacity/transparency                            #
 #######################################################################
 
 # powerlevel10k https://github.com/romkatv/powerlevel10k
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
-# p10k configure # if setup doesnt start automatically
+p10k configure
 
 # nvm with node
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.zshrc
 nvm install 22
 
@@ -58,16 +59,15 @@ nvm install 22
 # Software installation (optional)                                    #
 #######################################################################
 
-# brave-browser https://brave.com/linux/
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
-
 # vscode https://code.visualstudio.com/Download
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 sudo apt install apt-transport-https -y
-sudo apt install code
+sudo apt update && sudo apt install -y code
+
+# brave-browser https://brave.com/linux/
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update && sudo apt install -y brave-browser
